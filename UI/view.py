eccf6b0d4758a7be5e232e81ae4e6a0a68b2b0e3
cmas1 <car.masone@gmail.com>
2024-04-18 10:48:59 +0200

    progetto base, con vocabolario

--- UI/view.py
+++ UI/view.py
@@ -6,40 +6,78 @@ class View(ft.UserControl):
         super().__init__()
         # page stuff
         self._page = page
-        self._page.title = "Template application using MVC and DAO"
+        self._page.title = "Esercizio su ricorsione"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
+        self._page.theme_mode = ft.ThemeMode.LIGHT
         # controller (it is not initialized. Must be initialized in the main, after the controller is created)
         self._controller = None
         # graphical elements
         self._title = None
-        self.txt_name = None
-        self.btn_hello = None
-        self.txt_result = None
-        self.txt_container = None
+        self.txt_word = None
+        self.btn_anagrams = None
+        self.btn_reset = None
+        self.lst_correct = None
+        self.lst_wrong = None
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
+        self._title = ft.Text("Anagrammi", color="blue", size=24)
         self._page.controls.append(self._title)
 
-        #ROW with some controls
+        ##############################
+        # ROW with some controls
+        ##############################
         # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
+        self.txt_word = ft.TextField(
+            label="parola",
+            width=400,
+            hint_text="Inserire la parola da anagrammare"
         )
 
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
+        # button for anagrams
+        self.btn_anagrams = ft.ElevatedButton(text="Anagrammi", on_click=self._controller.calcola_anagrammi)
+        self.btn_reset = ft.ElevatedButton(text="Reset", on_click=self._controller.reset)
+        row1 = ft.Row([self.txt_word, self.btn_anagrams, self.btn_reset],
                       alignment=ft.MainAxisAlignment.CENTER)
         self._page.controls.append(row1)
 
+        #######################################
+        # Result areas placed in different tabs
+        #######################################
         # List View where the reply is printed
-        self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
-        self._page.controls.append(self.txt_result)
+        self.lst_correct = ft.ListView(expand=1,
+                                       spacing=10,
+                                       padding=20,
+                                       auto_scroll=True,
+                                       controls=[ft.Text("----")])
+        self.lst_wrong = ft.ListView(expand=1,
+                                     spacing=10,
+                                     padding=20,
+                                     auto_scroll=True,
+                                     controls=[ft.Text("----")])
+
+        tabs_results = ft.Tabs(
+            selected_index=0,
+            animation_duration=300,
+            tabs=[
+                ft.Tab(
+                    text="Anagrammi corretti",
+                    content=ft.Container(
+                        content=self.lst_correct,
+                        alignment=ft.alignment.center
+                    ),
+                ),
+                ft.Tab(
+                    text="Anagrammi errati",
+                    content=ft.Container(
+                        content=self.lst_wrong,
+                        alignment=ft.alignment.center
+                    ),
+                ),
+            ],
+            expand=1,
+        )
+        self._page.controls.append(tabs_results)
         self._page.update()
 
     @property
